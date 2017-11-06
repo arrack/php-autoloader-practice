a88e534517e9e9f3c76a95d9dd85477ca9880ca9
arrack <arrack@pixnet.tw>
2017-11-06 10:58:33 +0800

    add example/6.composer_priority

---
+++ example/6.composer_priority/index.php
@@ -0,0 +1,8 @@
+<?php
+require_once('vendor/autoload.php');
+
+$hello = new Arrack\Hello\Hello;
+echo $hello->world() . PHP_EOL;
+
+
+
---
+++ example/6.composer_priority/src/psr4/Hello.php
@@ -0,0 +1,8 @@
+<?php
+Namespace Arrack\Hello;
+echo 'load ' . __FILE__ . PHP_EOL;
+class Hello {
+    public function world() {
+        return 'Hello World PSR-4';
+    }
+}
